7a78692b55d534033f991032cd46a973a3ac4a12
hoonba <hunman90@naver.com>
2025-07-27 23:43:53 +0900

    add the image config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: "/hoon-portfolio",
   trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
